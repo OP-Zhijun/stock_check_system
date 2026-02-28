6ffa81d2accfaad1c4c789365ebb1cf194495db7
Wk-P <weekend2000119@gmail.com>
2026-03-01 00:23:31 +0900

    sync from OP-Zhijun

--- wsgi.py
+++ wsgi.py
@@ -3,11 +3,12 @@
 
 import sys
 import os
+from pathlib import Path
 
 # Add your project directory to the sys.path
-project_home = '/home/nanolab/stock_check_system'
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
+project_home = Path(__file__).parent
+if str(project_home) not in sys.path:
+    sys.path.insert(0, str(project_home))
 
 # Set the working directory
 os.chdir(project_home)
